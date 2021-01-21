3fecce11b93f13360b4517a2158a7bae941b3023
cbrianbet <cbrianbet@outlook.com>
2021-01-21 21:40:34 +0300

    changes to the forms

--- bills/views.py
+++ bills/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import *
 from properties.models import *
@@ -8,7 +8,7 @@ from properties.models import *
 @login_required
 def tenant_bills(request, u_uid):
     tenant = Tenant.objects.get(uuid=u_uid)
-    invoice = InvoiceItems.objects.filter(invoice__invoice_for=tenant.profile.user)
+    invoice = Invoice.objects.filter(invoice_for=tenant.profile.user)
     context = {
         'bills': invoice,
         'u_uid': u_uid,
@@ -18,13 +18,25 @@ def tenant_bills(request, u_uid):
 
 @login_required
 def invoice_tenant(request, u_uid):
+    t = Tenant.objects.get(uuid=u_uid)
     if request.method == "POST":
         print(request.POST)
         name = request.POST.getlist('inv_name')
-        amt = request.POST.getlist('inv_amount')
-        remarks = request.POST.getlist('remarks')
+        amt = request.POST.getlist('inv_amnt')
+        remarks = request.POST.getlist('remark')
+        for i in amt:
+            print(i)
+        inv_no = increment_invoice_number()
+        print(inv_no , remarks)
+        inv = Invoice.objects.create(created_by=request.user, invoice_no=inv_no, invoice_for=t.profile.user, unit=t.unit)
+        inv.save()
 
-        inv = Invoice.objects.all()
+        for i in range(len(name)):
+            inv_item = InvoiceItems.objects.create(invoice=inv, invoice_item=name[i], amount=float(amt[i]), description=remarks[i])
+            inv_item.save()
+
+        return redirect('bills-tenant', u_uid=u_uid)
+
     tenant = Tenant.objects.get(uuid=u_uid)
     invoice = InvoiceItems.objects.filter(invoice__invoice_for=tenant.profile.user)
     context = {
@@ -33,3 +45,14 @@ def invoice_tenant(request, u_uid):
         'user': request.user,
     }
     return render(request, 'bills/add_invoice.html', context)
+
+
+def increment_invoice_number():
+    last_invoice = Invoice.objects.all().order_by('id').last()
+    if not last_invoice:
+         return 'INV-00001'
+    invoice_no = last_invoice.invoice_no
+    invoice_int = int(invoice_no.split('INV-')[-1])
+    new_invoice_int = invoice_int + 1
+    new_invoice_no = 'INV-' + str('%05d' % new_invoice_int)
+    return new_invoice_no
