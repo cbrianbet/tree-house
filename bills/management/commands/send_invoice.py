3a69bdf074df2eb412947b7550aee5dcf94b4c92
cbrianbet <cbrianbet@outlook.com>
2021-01-29 11:39:52 +0300

    send invoice reminder 3 days before Command

--- bills/management/commands/send_invoice.py
+++ bills/management/commands/send_invoice.py
@@ -1,21 +1,100 @@
+import calendar
+from datetime import date
+
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand, CommandError
+
+from bills.models import RentInvoice, RentItems
+from bills.views import increment_rent_invoice_number
 from properties.models import Tenant
 from authapp.models import Users
+from tree_house.settings import EMAIL_HOST_USER
+
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Sends invoices out'
+
     #
     # def add_arguments(self, parser):
     #     parser.add_argument('poll_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
         try:
-            poll = Tenant.objects.filter()
-            print(poll)
+            tenant = Tenant.objects.all()
+            today = date.today().day
+            month = date.today()
+            last_day = calendar.monthrange(month.year, month.month)[1]
+            last_day = int(last_day)
+            today = int(today)
+            print(today, last_day)
+            for t in tenant:
+                print(t)
+                coll_day = 0
+                if t.unit.property.rent_collection == "Spec_date":
+                    coll_day = t.unit.property.specific_day
+                    coll_day = int(coll_day)
+                elif t.unit.property.rent_collection == "Occ_date":
+                    coll_day = t.date_occupied.date().day
+                    coll_day = int(coll_day)
+
+                if coll_day > 3:
+                    if coll_day > last_day:
+                        coll_day = last_day
+                    if today == (coll_day - 3):
+                        apply_invoice(t.unit.value, calendar.month_name[month], Users.objects.get(id=1), t,
+                                      '{}-{}-{}'.format(coll_day, calendar.month_name[month], month.year))
+                    elif coll_day == today:
+                        # TODO send reminder
+                        pass
+                elif coll_day == 0:
+                    # TODO extract day in prev month
+                    pass
+                elif coll_day < 3:
+                    # TODO extract day in prev month
+                    pass
+
+
+
         except Tenant.DoesNotExist:
-            raise CommandError('Poll  does not exist')
+            raise CommandError('Tenant does not exist')
 
             # poll.opened = False
             # poll.save()
             #
             # self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+
+
+def apply_invoice(rent, month, user, tenant, date):
+    try:
+        # for r in RentInvoice.objects.filter(email_inform=False):
+        #     send_email(r.invoice_for, date)
+        inv_no = increment_rent_invoice_number()
+        i = RentInvoice.objects.create(created_by=user, invoice_no=inv_no, invoice_for=tenant.profile.user,
+                                       unit=tenant.unit)
+        i.save()
+        inv_item1 = RentItems.objects.create(invoice=i, invoice_item='RENT FOR {}'.format(month),
+                                             amount=round(rent, 2),
+                                             description='RENT')
+        inv_item1.save()
+        i.email_inform = send_email(tenant, date)
+        i.save()
+        return True
+    except:
+        return False
+
+
+def send_email(t, date):
+    subject = "Invoice for {}".format(t.unit.property.property_name)
+    message = '''
+        Dear {}, 
+        This email is to let you know that a Rent Invoice has been created for your unit {} at {}.
+        login to your portal at	mnestafrica.com to view it under bills. The incoice is due on: {}  
+        Your username is : {} incase you forgot.'''.format(t.profile.first_name, t.unit.unit_name,
+                                                           t.unit.property.property_name, date,
+                                                           t.profile.user.username)
+    try:
+        send_mail(subject, message, EMAIL_HOST_USER, [t.profile.user.email], fail_silently=False)
+        return True
+    except:
+        print('failed')
+        return False
