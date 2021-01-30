ab48451a411d2114692e776b3e73ffcd43fe7ad0
cbrianbet <cbrianbet@outlook.com>
2021-01-30 23:35:47 +0300

    added some reports

--- reports/urls.py
+++ reports/urls.py
@@ -5,5 +5,7 @@ from . import  views
 
 urlpatterns = [
     path('users/all', views.all_users, name='all-users'),
+    path('properties/all', views.all_props, name='all-props'),
+    path('companies/all', views.all_comps, name='all-companies'),
 
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,16 +1,39 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
-from authapp.models import Users
+from authapp.models import Users, Profile
+from properties.models import Properties, Companies, CompanyProfile
 
 
 @login_required
 def all_users(request):
-    all_users = Users.objects.all()
+    all_users = Profile.objects.filter(Q(user__acc_type_id=2) | Q(user__acc_type_id=3) | Q(user__acc_type_id=5) )
 
     context = {
         'user': request.user,
         'list': all_users,
     }
     return render(request, 'reports/all_users.html', context)
+
+
+@login_required
+def all_props(request):
+    all_props = Properties.objects.all()
+
+    context = {
+        'user': request.user,
+        'list': all_props,
+    }
+    return render(request, 'reports/all_properties.html', context)
+
+
+@login_required
+def all_comps(request):
+    all_comp = CompanyProfile.objects.filter(user__acc_type_id=3)
+
+    context = {
+        'user': request.user,
+        'list': all_comp,
+    }
+    return render(request, 'reports/all_companies.html', context)
