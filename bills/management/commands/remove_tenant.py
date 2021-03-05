ead4ce526f9b9d3049bb16b05abd74346fd13aa0
cbrianbet <cbrianbet@outlook.com>
2021-03-05 11:48:12 +0300

    Update remove_tenant.py

--- bills/management/commands/remove_tenant.py
+++ bills/management/commands/remove_tenant.py
@@ -1,24 +1,12 @@
-import calendar
 import datetime
 from datetime import date
 
-from django.core.mail import send_mail
 from django.core.management.base import BaseCommand, CommandError
-from django.db.models import Sum
-
-from bills.models import RentInvoice, RentItems, RentItemTransaction, Invoice, InvoiceItems, InvoiceItemsTransaction
-from bills.views import increment_rent_invoice_number
-from properties.models import Tenant, Properties, Unit, TenantHistory, VacateNotice
-from authapp.models import Users, Profile
-from tree_house.settings import EMAIL_HOST_USER
+from properties.models import Unit, TenantHistory, VacateNotice
 
 
 class Command(BaseCommand):
     help = 'Vacate Tenant'
-
-    #
-    # def add_arguments(self, parser):
-    #     parser.add_argument('poll_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
         try:
