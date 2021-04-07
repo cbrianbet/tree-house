294dde8aad852904a47dff832ee2c90283145646
Brian <cbrianbet@outlook.com>
2021-04-07 12:19:19 +0300

    added inactivity check for tenants

--- bills/management/commands/remove_tenant.py
+++ bills/management/commands/remove_tenant.py
@@ -2,6 +2,8 @@ import datetime
 from datetime import date
 
 from django.core.management.base import BaseCommand, CommandError
+
+from authapp.models import Users
 from properties.models import Unit, TenantHistory, VacateNotice
 
 
@@ -18,6 +20,9 @@ class Command(BaseCommand):
                 unit = Unit.objects.get(id=prop.curr_unit.id)
                 unit.unit_status = "Vacant"
                 unit.save()
+                user = Users.objects.get(id=v.notice_from.profile.user_id)
+                user.is_active = False
+                user.save()
 
         except:
             raise CommandError('Tenant does not exist')
